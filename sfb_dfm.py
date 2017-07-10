91ceac0207ce51dc6e34ff380bb2db718cc72a72
Rusty Holleman <rustyh@sfei.org>
2017-07-10 08:47:05 -0700

    seems to be working

--- sfb_dfm.py
+++ sfb_dfm.py
@@ -16,6 +16,8 @@ import six
 from stompy import utils
 import stompy.model.delft.io as dio
 from stompy.model.delft import dfm_grid
+from stompy.spatial import wkb2shp
+from stompy.io.local import usgs_nwis,noaa_coops
 
 log=logging.getLogger('sfb_dfm')
 log.setLevel(logging.INFO)
@@ -25,7 +27,7 @@ DAY=np.timedelta64(86400,'s') # useful for adjusting times
 ## --------------------------------------------------
 
 # Parameters to control more specific aspects of the run
-run_name="short_20120801" 
+run_name="short_20120801_p24" 
 run_start=np.datetime64('2012-08-01')
 run_stop=np.datetime64('2012-08-10')
 
@@ -48,30 +50,55 @@ ref_date=run_start.astype('datetime64[D]')
 
 net_file = os.path.join(base_dir,'sfei_v18_net.nc')
 
+new_bc_fn = os.path.join(run_base_dir,'FlowFMnew_bnd.ext')
+old_bc_fn = os.path.join(run_base_dir,'FlowFMold_bnd.ext')
+
+obs_shp_fn = os.path.join(base_dir,'inputs-static','observation-points.shp')
+
 ##
 
 # Make sure run directory exists:
 os.path.exists(run_base_dir) or os.makedirs(run_base_dir)
 
+# and that the bc files are clear:
+for fn in [new_bc_fn,old_bc_fn]:
+    os.path.exists(fn) and os.unlink(fn)
+
 ##
 
-if 1: # fetch wind data, write to format supported by DFM
-    target_filename_base=os.path.join(run_base_dir,"wind") # will have amu, amv appended
+if 1:
+    if 1:# fetch wind data, write to format supported by DFM
+        target_filename_base=os.path.join(run_base_dir,"wind") # will have amu, amv appended
 
-    if (os.path.exists(target_filename_base+".amu") and
-        os.path.exists(target_filename_base+".amv")):
-        log.info('Wind files already exist')
-    else:
-        
-        data_start=run_start-1*DAY
-        data_stop=run_stop+1*DAY
+        wind_u_fn=target_filename_base+".amu"
+        wind_v_fn=target_filename_base+".amv"
+        if (os.path.exists(wind_u_fn) and
+            os.path.exists(wind_v_fn)):
+            log.info('Wind files already exist')
+        else:
+            data_start=run_start-1*DAY
+            data_stop=run_stop+1*DAY
 
-        url='http://sfbaynutrients.sfei.org/erddap/griddap/wind_ludwig_20170621'
-        ds=xr.open_dataset(url)
+            url='http://sfbaynutrients.sfei.org/erddap/griddap/wind_ludwig_20170621'
+            ds=xr.open_dataset(url)
 
-        dio.dataset_to_dfm_wind(ds,data_start,data_stop,target_filename_base,
-                                extra_header="# downloaded from %s"%url)
-
+            dio.dataset_to_dfm_wind(ds,data_start,data_stop,target_filename_base,
+                                    extra_header="# downloaded from %s"%url)
+    if 1: # and add wind to the boundary forcing
+        wind_stanza=["QUANTITY=windx",
+                     "FILENAME=wind.amu",
+                     "FILETYPE=4",
+                     "METHOD=2",
+                     "OPERAND=O",
+                     "",
+                     "QUANTITY=windy",
+                     "FILENAME=wind.amv",
+                     "FILETYPE=4",
+                     "METHOD=2",
+                     "OPERAND=O",
+                     "\n"]
+        with open(old_bc_fn,'at') as fp:
+            fp.write("\n".join(wind_stanza))
 
 ##
 
@@ -160,7 +187,7 @@ if 1: #  Freshwater flows from git submodule
     # Use the "new" format for FlowFM_bnd.ext, writing one file, with a stanza
     # for each boundary condition, all referencing the same forcingfile, but
     # with the features split out to individual pli files.
-    with open(os.path.join(run_base_dir,'FlowFMnew_bnd.ext'),'wt') as fp:
+    with open(new_bc_fn,'at') as fp:
         for feat in pli_data:
             # This is how things were written out for hyperpycnal:
             # the same forcingfile can be specified multiple times, and it looks like
@@ -194,7 +221,7 @@ if 1:
     time_pnts = time_pnts.clip(0,len(potws.time)-1)
     time_idxs=range(time_pnts[0],time_pnts[1]) # enumerate them for loops below
 
-    with open(os.path.join(run_base_dir,'FlowFMold_bnd.ext'),'wt') as fp:
+    with open(old_bc_fn,'at') as fp:
         for site in potws.site.values:
             # NB: site is bytes at this point
             potw=potws.sel(site=site)
@@ -204,9 +231,9 @@ if 1:
                 site_s=site
 
             if site_s in ['false_sac','false_sj']:
-                print("Skipping %s"%site_s)
+                print("(skip %s) "%site_s,end="")
                 continue
-            print(site_s)
+            print("%s "%site_s,end="")
 
             fp.write( ("QUANTITY=discharge_salinity_temperature_sorsin\n"
                        "FILENAME=%s.pli\n"
@@ -283,101 +310,195 @@ if 1:
 # Sea_sal: constant 33
 # Sea_0001.pli - 
 
-# Per Silvia's Thesis:
+if 1: # Fetch river USGS river flows, add to FlowFM_bnd.ext:
+    # Per Silvia's Thesis:
+    # Jersey: Discharge boundary affected by tides, discharge and temperature taken
+    # from USGS 11337190 SAN JOAQUIN R A JERSEY POINT, 0 salinity
+    # (Note that Dutch Slough should probably be added in here)
+    # Rio Vista: 11455420 SACRAMENTO A RIO VISTA, temperature from DWR station RIV.
+    # 0 salinity.
+    
+    if 1: 
+        # Cache the original data from USGS, then clean it and write to DFM format
+        jersey_raw_fn=os.path.join(run_base_dir,'jersey-raw.nc')
+        if not os.path.exists(jersey_raw_fn):
+            jersey_raw=usgs_nwis.nwis_dataset(station="11337190",
+                                              start_date=run_start,end_date=run_stop,
+                                              products=[60, # "Discharge, cubic feet per second"
+                                                        10], # "Temperature, water, degrees Celsius"
+                                              days_per_request=30)
+            jersey_raw.to_netcdf(jersey_raw_fn,engine='scipy')
 
-# Jersey: Discharge boundary affected by tides, discharge and temperature taken
-# from USGS 11337190 SAN JOAQUIN R A JERSEY POINT, 0 salinity
-# (Note that Dutch Slough should probably be added in here)
+        rio_vista_raw_fn=os.path.join(run_base_dir,'rio_vista-raw.nc')
+        if not os.path.exists(rio_vista_raw_fn):
+            rio_vista_raw=usgs_nwis.nwis_dataset(station="11455420",
+                                                 start_date=run_start,end_date=run_stop,
+                                                 products=[60, # "Discharge, cubic feet per second"
+                                                           10], # "Temperature, water, degrees Celsius"
+                                                 days_per_request=30)
+            rio_vista_raw.to_netcdf(rio_vista_raw_fn,engine='scipy')
 
-# Rio Vista: 11455420 SACRAMENTO A RIO VISTA, temperature from DWR station RIV.
-# 0 salinity.
+
+    if 1: # clean the data
+        jersey_raw=xr.open_dataset(jersey_raw_fn)
+        rio_vista_raw=xr.open_dataset(rio_vista_raw_fn)
+
+        # fill any missing data via linear interpolation
+        def fill_data(da):
+            valid=np.isfinite(da.values)
+            valid_times=da.time[valid]
+            dt=np.median(np.diff(da.time))
+            dt_gap = np.diff(da.time[valid]).max()
+            if dt_gap > dt:
+                log.warning("%s: gaps up to %.1f minutes"%(da.name, dt_gap/np.timedelta64(60,'s')))
+            da.values = utils.fill_invalid(da.values)    
+
+        fill_data(jersey_raw.stream_flow_mean_daily)
+        fill_data(jersey_raw.temperature_water)
+
+        fill_data(rio_vista_raw.stream_flow_mean_daily)
+        fill_data(rio_vista_raw.temperature_water)
+
+    if 1: # Write it all out
+        for src_name,source in [ ('Jersey',jersey_raw),
+                                 ('RioVista',rio_vista_raw)]:
+            # Add a stanze to FlowFMnew_bnd.ext:
+            with open(new_bc_fn,'at') as fp:
+                for quant in ['dischargebnd','temperaturebnd']:
+                    fp.write("\n".join(["\n[boundary]",
+                                        "quantity=%s"%quant,
+                                        "locationfile=%s.pli"%src_name,
+                                        "forcingfile=%s.bc"%src_name,
+                                        "return_time=0 # disable thatcher-harlemann",
+                                        ""]))
+
+            # Write the data:
+            df=source.to_dataframe().reset_index()
+            df['unix_time']=utils.to_unix(df.time.values)
+            df['flow_cms']=0.028316847 * df['stream_flow_mean_daily']
+            with open(os.path.join(run_base_dir,'%s.bc'%src_name),'wt') as bc_fp:
+                for quant,units,column in [ ('dischargebnd','m3 s-1','flow_cms'),
+                                            ('temperaturebnd','degC','temperature_water') ]:
+                    bc_fp.write("\n".join( [ "[forcing]",
+                                             # 'Name' needs to match the name in the pli
+                                             "Name               = %s_0001"%src_name,
+                                             "Function           = timeseries",
+                                             "Time-interpolation = linear",
+                                             "Quantity           = time",
+                                             "Unit               = seconds since 1970-01-01 00:00:00",
+                                             "Quantity           = %s"%quant,
+                                             "Unit               = %s"%units,  # was "m³/s\n", but prefer simpler characters
+                                             "\n"]))
+
+                    df.to_csv(bc_fp, sep=' ', index=False, header=False, columns=['unix_time', column])
+
+            # And copy the pli:
+            jersey_pli   =dio.read_pli(os.path.join(base_dir,'inputs-static','Jersey.pli'))
+            rio_vista_pli=dio.read_pli(os.path.join(base_dir,'inputs-static','RioVista.pli'))
+
+            dio.write_pli(os.path.join(run_base_dir,'Jersey.pli'  ), jersey_pli)
+            dio.write_pli(os.path.join(run_base_dir,'RioVista.pli'), rio_vista_pli)
+
+## 
 
 # Ocean:
 #  Water level data from station 46214 (apparently from Yi Chao's ROMS?)
 #    no spatial variation
-#  Maybe salinity from Yi Chao ROMS.
+#  Maybe salinity from Yi Chao ROMS?  That's what the thesis says, but the
+#  actual inputs look like constant 33
 
-# Fetch river USGS river flows, add to FlowFM_bnd.ext:
+if 1: # Ocean BCs from Point Reyes
+    ptreyes_raw_fn=os.path.join(run_base_dir,'ptreyes-raw.nc')
+    if 1:
+        if not os.path.exists(ptreyes_raw_fn):
+            ptreyes_raw=noaa_coops.coops_dataset("9415020",run_start,run_stop,
+                                                 ["water_level","water_temperature"],
+                                                 days_per_request=30)
 
-# Cache the original data from USGS, then clean it and write to DFM format
-jersey_raw_fn=os.path.join(run_base_dir,'jersey-raw.nc')
-if not os.path.exists(jersey_raw_fn):
-    jersey_raw=usgs_nwis.nwis_dataset(station="11337190",
-                                      start_date=run_start,end_date=run_stop,
-                                      products=[60, # "Discharge, cubic feet per second"
-                                                10], # "Temperature, water, degrees Celsius"
-                                      days_per_request=30)
-    jersey_raw.to_netcdf(jersey_raw_fn,engine='scipy')
+            ptreyes_raw.to_netcdf(ptreyes_raw_fn,engine='scipy')
 
-rio_vista_raw_fn=os.path.join(run_base_dir,'rio_vista-raw.nc')
-if not os.path.exists(rio_vista_raw_fn):
-    rio_vista_raw=usgs_nwis.nwis_dataset(station="11455420",
-                                         start_date=run_start,end_date=run_stop,
-                                         products=[60, # "Discharge, cubic feet per second"
-                                                   10], # "Temperature, water, degrees Celsius"
-                                         days_per_request=30)
-    rio_vista_raw.to_netcdf(rio_vista_raw_fn,engine='scipy')
+    if 1:
+        # Clean that up, fabricate salinity
+        ptreyes=xr.open_dataset(ptreyes_raw_fn)
+
+        # fill any missing data via linear interpolation
+        def fill_data(da):
+            valid=np.isfinite(da.values)
+            valid_times=da.time[valid]
+            dt=np.median(np.diff(da.time))
+            dt_gap = np.diff(da.time[valid]).max()
+            if dt_gap > dt:
+                log.warning("%s: gaps up to %.1f minutes"%(da.name, dt_gap/np.timedelta64(60,'s')))
+            da.values = utils.fill_invalid(da.values)    
+
+        fill_data(ptreyes.water_level)
+        fill_data(ptreyes.water_temperature)
+
+        ptreyes['salinity']=33 + 0*ptreyes.water_temperature
+
+    if 1: # Write it all out
+        # Add a stanze to FlowFMnew_bnd.ext:
+        src_name='Sea'
+        source=ptreyes
+        forcing_data=[('waterlevelbnd','m','water_level'),
+                      ('salinitybnd','ppt','salinity'),
+                      ('temperaturebnd','degC','water_temperature')]
+        
+        with open(new_bc_fn,'at') as fp:
+            for quant,_,_ in forcing_data:
+                fp.write("\n".join(["\n[boundary]",
+                                    "quantity=%s"%quant,
+                                    "locationfile=%s.pli"%src_name,
+                                    "forcingfile=%s.bc"%src_name,
+                                    "return_time=0 # disable thatcher-harlemann",
+                                    ""]))
+
+        # Write the data:
+        df=source.to_dataframe().reset_index()
+        df['unix_time']=utils.to_unix(df.time.values)
+
+        with open(os.path.join(run_base_dir,'%s.bc'%src_name),'wt') as bc_fp:
+            for quant,units,column in forcing_data:
+                txt="\n".join(["[forcing]",
+                               # 'Name' needs to match the name in the pli
+                               "Name               = %s_0001"%src_name,
+                               "Function           = timeseries",
+                               "Time-interpolation = linear",
+                               "Quantity           = time",
+                               "Unit               = seconds since 1970-01-01 00:00:00",
+                               "Quantity           = %s"%quant,
+                               "Unit               = %s"%units,  # was "m³/s\n", but prefer simpler characters
+                               ""])
+                bc_fp.write(txt)
+                df.to_csv(bc_fp, sep=' ', index=False, header=False,
+                          columns=['unix_time', column])
+                bc_fp.write("\n")
+
+        # And copy the pli:
+        sea_pli   =dio.read_pli(os.path.join(base_dir,'inputs-static','Sea.pli'))
+
+        dio.write_pli(os.path.join(run_base_dir,'Sea.pli'), sea_pli)
 
 ##
 
-jersey_raw=xr.open_dataset(jersey_raw_fn)
-rio_vista_raw=xr.open_dataset(rio_vista_raw_fn)
+# Spatial salinity initial condition and friction
+if 1:
+    txt="\n".join([ "QUANTITY=initialsalinity",
+                    "FILENAME=%s/saltopini.xyz"%static_dir,
+                    "FILETYPE=7",
+                    "METHOD=5",
+                    "OPERAND=O",
+                    "",
+                    "QUANTITY=frictioncoefficient",
+                    "FILENAME=%s/friction12e.xyz"%static_dir,
+                    "FILETYPE=7",
+                    "METHOD=5",
+                    "OPERAND=O",
+                    ""])
+    with open(old_bc_fn,'at') as fp:
+        fp.write(txt)
 
-## 
-# fill any missing data via linear interpolation
-def fill_data(da):
-    valid=np.isfinite(da.values)
-    valid_times=da.time[valid]
-    dt=np.median(np.diff(da.time))
-    dt_gap = np.diff(da.time[valid]).max()
-    if dt_gap > dt:
-        log.warning("%s: gaps up to %.1f minutes"%(da.name, dt_gap/np.timedelta64(60,'s')))
-    da.values = utils.fill_invalid(da.values)    
-
-fill_data(jersey_raw.stream_flow_mean_daily)
-fill_data(jersey_raw.temperature_water)
-
-fill_data(rio_vista_raw.stream_flow_mean_daily)
-fill_data(rio_vista_raw.temperature_water)
-
-##
-
-# Write those out --
-for src_name,source in [ ('Jersey',jersey_raw),
-                         ('RioVista',rio_vista_raw)]:
-    # Add a stanze to FlowFMnew_bnd.ext:
-    with open(os.path.join(run_base_dir,'FlowFMnew_bnd.ext'),'at') as fp:
-        for quant in ['dischargebnd','temperaturebnd']:
-            fp.write("\n".join(["\n[boundary]",
-                                "quantity=%s"%quant,
-                                "locationfile=%s.pli"%src_name,
-                                "forcingfile=%s.bc"%src_name,
-                                "return_time=0 # disable thatcher-harlemann",
-                                ""]))
-    
-    # Write the data:
-    df=source.to_dataframe().reset_index()
-    df['unix_time']=utils.to_unix(df.time.values)
-    df['flow_cms']=0.028316847 * df['stream_flow_mean_daily']
-    with open(os.path.join(run_base_dir,'%s.bc'%src_name),'wt') as bc_fp:
-        bc_fp.write("[forcing]\n")
-        # 'Name' needs to match the name in the pli
-        bc_fp.write("Name               = %s_0001\n"%src_name)
-        bc_fp.write("Function           = timeseries\n")
-        bc_fp.write("Time-interpolation = linear\n")
-        bc_fp.write("Quantity           = time\n")
-        bc_fp.write("Unit               = seconds since 1970-01-01 00:00:00\n")
-        bc_fp.write("Quantity           = dischargebnd\n")
-        bc_fp.write("Unit               = m3 s-1\n")  # was "m³/s\n", but prefer simpler characters
-        bc_fp.write("Quantity           = temperaturebnd\n")
-        bc_fp.write("Unit               = degC\n")
-
-        df.to_csv(bc_fp, sep=' ', index=False, header=False, columns=['unix_time', 'flow_cms','temperature_water'])
-
-    # And copy the pli:
-    
-
-## 
+## --------------------------------------------------------------------------------
 # Edits to the template mdu:
 # 
 
@@ -399,26 +520,35 @@ if 1: # set dates
     mdu['time','TStart'] = 0
     mdu['time','TStop'] = int( (run_stop - run_start) / np.timedelta64(1,'m') )
 
-
 if 1: # update location of the boundary conditions
     # this has the source/sinks which cannot be written in the new style file
-    mdu['external forcing','ExtForceFile']="FlowFMold_bnd.ext"
+    mdu['external forcing','ExtForceFile']=os.path.basename(old_bc_fn)
     # this has boundary flows
-    mdu['external forcing','ExtForceFileNew']="FlowFMnew_bnd.ext"
+    mdu['external forcing','ExtForceFileNew']=os.path.basename(new_bc_fn)
 
 if 0: #
     # Would be adding evaporation as negative rain here.
     pass
 
+
+
 if 1: # output locations
-    mdu['output','ObsFile'] = os.path.join(static_dir,"Southbay-withoutDelta_obs.xyn")
     mdu['output','CrsFile'] = os.path.join(static_dir,"SB-observationcrosssection.pli")
 
-
+    # Observation points taken from shapefile for easier editing/comparisons in GIS
+    
+    # mdu['output','ObsFile'] = os.path.join(static_dir,"Southbay-withoutDelta_obs.xyn")
+##
+if 1:    
+    obs_pnts=wkb2shp.shp2geom(obs_shp_fn)
+    obs_fn='observation_pnts.xyn'
+    
+    with open(os.path.join(run_base_dir,obs_fn),'wt') as fp:
+        for idx,row in enumerate(obs_pnts):
+            xy=np.array(row['geom'])
+            fp.write("%12g %12g '%s'\n"%(xy[0], xy[1], row['name']))
+    mdu['output','ObsFile'] = obs_fn
+    
+## 
 mdu.write(os.path.join(run_base_dir,run_name+".mdu"))
 
-# At this point dflowfm will not run on it (low-level error),
-# and it does not appear that the boundary conditions are read properly.
-
-# But - Delta Shell, *will* read it.
-# The boundary conditions come in okay.
